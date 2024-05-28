0136e30ccf28fb51ae72a04bf1e2a7b2a5a88c00
vitoria.sousa <maria.v.cosmo@gmail.com>
2024-05-28 20:25:14 -0300

    metodos js

--- script.js
+++ script.js
@@ -2,16 +2,26 @@ const html = document.querySelector('html');
 const focoBt = document.querySelector('.app__card-button--foco');
 const curtoBt = document.querySelector('.app__card-button--curto');
 const longoBt = document.querySelector('.app__card-button--longo');
+const banner = document.querySelector('.app__image');
 
 focoBt.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'foco');
+    alterarContexto('foco');
+    
 })
 
 curtoBt.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'descanso-curto');
+    alterarContexto('descanso-curto');
+    
 })
 
 longoBt.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'descanso-longo');
+    alterarContexto('descanso-longo');
+    
 })
 
+function alterarContexto(contexto) {
+    html.setAttribute('data-contexto', contexto);
+    banner.setAttribute('src', `/imagens/${contexto}.png`);
+    
+    
+}
