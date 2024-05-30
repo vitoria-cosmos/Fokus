39702258cf6e051b79e05890bc8ebfcb75223165
vitoria.sousa <maria.v.cosmo@gmail.com>
2024-05-29 23:00:01 -0300

    learning about innerHTML

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const focoBt = document.querySelector('.app__card-button--foco');
 const curtoBt = document.querySelector('.app__card-button--curto');
 const longoBt = document.querySelector('.app__card-button--longo');
 const banner = document.querySelector('.app__image');
+const titulo = document.querySelector('.app__title');
 
 focoBt.addEventListener('click', () => {
     alterarContexto('foco');
@@ -22,6 +23,13 @@ longoBt.addEventListener('click', () => {
 function alterarContexto(contexto) {
     html.setAttribute('data-contexto', contexto);
     banner.setAttribute('src', `/imagens/${contexto}.png`);
+    switch (contexto) {
+        case "foco":
+            titulo.innerHTML = `
+            Otimize sua produtividade, <br> <stront class="app__title-strong">mergulhe no que importa.</strong>`;
+            break;
+        case: 
+    }
     
     
 }
