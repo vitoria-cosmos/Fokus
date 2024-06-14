76eebb7fa3283f345db50dd50f772b632ae5417d
vitoria.sousa <maria.v.cosmo@gmail.com>
2024-06-14 09:53:38 -0300

    adicionando o tempo para cada contexto e finalizando o projeto

--- script.js
+++ script.js
@@ -31,24 +31,28 @@ musicaFocoInput.addEventListener('change', () => {
     }
 });
 focoBt.addEventListener('click', () => {
+    tempoDecorridoEmSegundos = 1500;
     alterarContexto('foco');
     focoBt.classList.add('active');
     
 });
 
 curtoBt.addEventListener('click', () => {
+    tempoDecorridoEmSegundos = 300;
     alterarContexto('descanso-curto');
     curtoBt.classList.add('active');
     
 });
 
 longoBt.addEventListener('click', () => {
+    tempoDecorridoEmSegundos = 900;
     alterarContexto('descanso-longo');
     longoBt.classList.add('active');
     
 });
 
 function alterarContexto(contexto) {
+    mostrarTempo();
     botoes.forEach(function (botao) {
         botao.classList.remove('active');
     });
@@ -111,8 +115,9 @@ function zerar() {
 }
 
 function mostrarTempo() {
-    const tempo = tempoDecorridoEmSegundos;
-    tempoNaTela.innerHTML = `${tempo}`;
+    const tempo = new Date(tempoDecorridoEmSegundos * 1000);
+    const tempoFormatado = tempo.toLocaleTimeString('pt-Br', {minute: '2-digit', second: '2-digit'});
+    tempoNaTela.innerHTML = `${tempoFormatado}`;
 }
 
 mostrarTempo();
