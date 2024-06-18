4f8056c90c29c9980d6dbbd68e710f93e215b154
vitoria.sousa <maria.v.cosmo@gmail.com>
2024-06-17 23:52:46 -0300

    adicionando localStorage

---
+++ script-crud.js
@@ -0,0 +1,20 @@
+// encontrar botão adicionar tarefa 
+
+const btnAdicionarTarefa = document.querySelector('.app__button--add-task');
+const formAdicionarTarefa = document.querySelector('.app__form-add-task');
+const textArea = document.querySelector('.app__form-textarea');
+const tarefas = [];
+
+btnAdicionarTarefa.addEventListener('click', () => {
+    formAdicionarTarefa.classList.toggle('hidden');
+})
+//  nesta função pegamos o evento para ter controle sobre o formulário 
+formAdicionarTarefa.addEventListener('submit', (evento) => {
+    evento.preventDefault(); // Faz com que quando eu aperto em salvar, o formulário não seja recarregado.
+    // const descricaoTarefa = textArea.value; aqui ele pega o que o usuário digitou
+    const tarefa = { // esse objeto identifica a tarefa que estou cadastrando no momento.
+        descricao: textArea.value
+    }
+    tarefas.push(tarefa);
+    localStorage.setItem('tarefas', tarefas);
+})
