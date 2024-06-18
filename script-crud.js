0a3f58e7db0c1f6e8919fd149754231e566423e7
vitoria.sousa <maria.v.cosmo@gmail.com>
2024-06-18 16:14:58 -0300

    Adicionando a API JSON

--- script-crud.js
+++ script-crud.js
@@ -10,11 +10,12 @@ btnAdicionarTarefa.addEventListener('click', () => {
 })
 //  nesta função pegamos o evento para ter controle sobre o formulário 
 formAdicionarTarefa.addEventListener('submit', (evento) => {
-    evento.preventDefault(); // Faz com que quando eu aperto em salvar, o formulário não seja recarregado.
+    evento.preventDefault(); // Faz com que quando eu aperto em salvar, o formulário não seja enviado naturalmente.
     // const descricaoTarefa = textArea.value; aqui ele pega o que o usuário digitou
     const tarefa = { // esse objeto identifica a tarefa que estou cadastrando no momento.
         descricao: textArea.value
     }
     tarefas.push(tarefa);
-    localStorage.setItem('tarefas', tarefas);
+    // localStorage.setItem('tarefas', tarefas); o local storage só aceita string, por isso temos que transformar a lista em string.
+    localStorage.setItem('tarefas', JSON.stringify(tarefas));
 })
